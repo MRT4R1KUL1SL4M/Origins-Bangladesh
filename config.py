487abadf1cce7dad2043b51d485b1cbea28c4feb
Md. Tarikul Islam <141022752+MRT4R1KUL1SL4M@users.noreply.github.com>
2026-04-04 19:56:55 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "1046")
+    DB_HOST = os.getenv("DB_HOST", "gateway01.ap-southeast-1.prod.aws.tidbcloud.com")
+    DB_USER = os.getenv("DB_USER", "2cPo7b5N2rLWuv9.root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "P6S0HBGaSQY1gw74")
     DB_NAME = os.getenv("DB_NAME", "origins_bangladesh")
 
 
@@ -15,4 +15,4 @@ class Config:
     SMTP_USER = os.getenv("SMTP_USER", "compilebreakers@gmail.com")
     SMTP_PASS = os.getenv("SMTP_PASS", "wngz bbfr mght hscq")
     SMTP_FROM = os.getenv("SMTP_FROM", SMTP_USER or "no-reply@example.com")
-    BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:5000")  # e.g. https://yourdomain.com/
+    BASE_URL = os.getenv("BASE_URL", "https://origins-bangladesh.onrender.com")
